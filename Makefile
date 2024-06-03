6b18e44aa1c89902ee517f1d7bbc9f7d8fbbc3a0
assilbekov <askhat.assilbekov@gmail.com>
2024-06-03 13:08:49 -0300

    list features update

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ genproto:
 start_server:
 	go run cmd/grpc_server/main.go
 
+start_streaming_server:
+	go run cmd/grpc_streaming_server/main.go
+
 start_client:
 	go run cmd/grpc_client/main.go
--- cmd/grpc_streaming_server/main.go
+++ cmd/grpc_streaming_server/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
 	"grpc-simple-server-client-example/api/proto/streaming_example"
 	"log"
+	"math"
 	"net"
 	"sync"
 )
@@ -32,10 +34,67 @@ func (r *routeGuideServer) GetFeature(
 	return &streaming_example.Feature{Location: point}, nil
 }
 
+func (r *routeGuideServer) ListFeatures(
+	rect *streaming_example.Rectangle,
+	stream streaming_example.RouteGuide_ListFeaturesServer,
+) error {
+	for _, feature := range r.savedFeatures {
+		if inRange(feature.Location, rect) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 func (r *routeGuideServer) loadFeatures() {
 	if err := json.Unmarshal(exampleData, &r.savedFeatures); err != nil {
 		log.Fatalf("Failed to load default features: %v", err)
 	}
+}
+
+func inRange(point *streaming_example.Point, rect *streaming_example.Rectangle) bool {
+	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+
+	if float64(point.Longitude) >= left &&
+		float64(point.Longitude) <= right &&
+		float64(point.Latitude) >= bottom &&
+		float64(point.Latitude) <= top {
+		return true
+	}
+
+	return false
+}
+
+func toRadians(num float64) float64 {
+	return num * math.Pi / float64(180)
+}
+
+func calculateDistance(p1, p2 *streaming_example.Point) int32 {
+	const CordFactor float64 = 1e7
+	const R = float64(6371000) // earth radius in metres
+	lat1 := toRadians(float64(p1.Latitude) / CordFactor)
+	lat2 := toRadians(float64(p2.Latitude) / CordFactor)
+	lng1 := toRadians(float64(p1.Longitude) / CordFactor)
+	lng2 := toRadians(float64(p2.Longitude) / CordFactor)
+	dlat := lat2 - lat1
+	dlng := lng2 - lng1
+
+	a := math.Sin(dlat/2)*math.Sin(dlat/2) +
+		math.Cos(lat1)*math.Cos(lat2)*
+			math.Sin(dlng/2)*math.Sin(dlng/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	distance := R * c
+	return int32(distance)
+}
+
+func serialize(point *streaming_example.Point) string {
+	return fmt.Sprintf("%d %d", point.Latitude, point.Longitude)
 }
 
 func main() {
